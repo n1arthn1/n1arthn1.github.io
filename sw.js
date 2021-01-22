bf1907cdc2ccc6563a31b80ba9f112f2998e8a52
arturn1 <artur.a.patricio@gmail.com>
2021-01-22 04:57:55 -0300

    typo

--- sw.js
+++ sw.js
@@ -1,17 +1,12 @@
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-      caches.open('cache').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/js/app.js',
-          '/js/chunk-vendors.js',
-          'img/ali.7e3a582f.webp',
-          'img/book.bbb0dfa5.webp',
-          'img/sports.ec56aa0f.webp',
-          'img/e-sports.21aea442.png',
-          '/img/email-svgrepo-com.cee522e3.svg',
-          '/img/whatsapp-svgrepo-com.9ada85f9.svg'
-        ]);
+importScripts('serviceworker-cache-polyfill.js');
+   
+   self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.open('mysite-dynamic').then(function(cache) {
+        return fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
       })
     );
-   });
+  });
