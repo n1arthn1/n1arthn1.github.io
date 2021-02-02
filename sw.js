0b09584c4bd7956c7050882bd72982836030fc85
arturn1 <artur.a.patricio@gmail.com>
2021-02-02 02:24:03 -0300

    update skilss

--- sw.js
+++ sw.js
@@ -1,11 +1,10 @@
-/*
 importScripts('serviceworker-cache-polyfill.js');
 
 this.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(resp) {
       return resp || fetch(event.request).then(function(response) {
-        return caches.open('v1').then(function(cache) {
+        return caches.open('assets').then(function(cache) {
           cache.put(event.request, response.clone());
           return response;
         });
@@ -13,5 +12,29 @@ this.addEventListener('fetch', function(event) {
     })
   );
 });
+
+/*
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);})
    
+
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('cache-arth').then(function(cache) {
+      return cache.addAll([
+        '/sw-test/',
+        '/sw-test/index.html',
+        '/sw-test/style.css',
+        '/sw-test/app.js',
+        '/sw-test/image-list.js',
+        '/sw-test/star-wars-logo.jpg',
+        '/sw-test/gallery/',
+        '/sw-test/gallery/bountyHunters.jpg',
+        '/sw-test/gallery/myLittleVader.jpg',
+        '/sw-test/gallery/snowTroopers.jpg'
+      ]);
+    })
+  );
+});
+
 */
